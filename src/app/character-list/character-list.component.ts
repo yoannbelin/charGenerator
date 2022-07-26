6d491b51d777a019f218a7dae19dc044080a19d0
yoannbelin <yoann.belin@icloud.com>
2022-07-26 10:24:32 +0200

    add animation on list-characters (WIP)

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, style, transition, trigger, state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Character } from '../character';
 import { CharacterService } from '../character.service';
@@ -7,6 +7,18 @@ import { CharacterService } from '../character.service';
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
   styleUrls: ['./character-list.component.css'],
+  animations: [
+    trigger('hoverNothover', [
+      state('hover', style({ left: '-50px' })),
+      state('notHover', style({ left: 0 })),
+      transition('notHover => hover', [
+        animate('1s')
+      ]),
+      transition('hover => notHover', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 export class CharacterListComponent implements OnInit {
 
@@ -24,5 +36,7 @@ export class CharacterListComponent implements OnInit {
     this.characterService.getCharacters()
     .subscribe(characters => this.characters = characters);
   }
+
+  isHover = false;
 
 }
