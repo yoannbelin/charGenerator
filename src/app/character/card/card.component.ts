cd87dbfa1934475bf984d409d92be3a81162e69c
yoannbelin <yoann.belin@icloud.com>
2022-07-27 00:59:19 +0200

    Done: mise en place de l'animation des cards personnages au hover / WIP: Maintient de l'ouverture de la card au survol du bouton suppression

--- src/app/character/card/card.component.ts
+++ src/app/character/card/card.component.ts
@@ -7,21 +7,26 @@ import { Character } from 'src/app/character';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
   animations: [
-    trigger('hoverNothover', [
+    trigger('parentAnimation', [
+      transition('notHover => hover', [
+        query('@test2', [animateChild()]),
+        query('@test', [animateChild()])
+      ])
+    ]),
+    trigger('test2', [
       state('hover', style({ left: '-50px' })),
       state('notHover', style({ left: 0 })),
-      transition('notHover <=> hover', [
-        animate('0.2s'),
-        query('@test', animateChild()),
+      transition("notHover => hover", [
+        animate('0.2s')
       ])
     ]),
     trigger('test', [
       state('hover', style({ right: '-51px' })),
       state('notHover', style({ right: 0 })),
-      transition("notHover <=> hover", [
+      transition("notHover => hover", [
         animate('0.2s')
       ])
-    ])
+    ]),
   ],
 })
 export class CardComponent implements OnInit {
